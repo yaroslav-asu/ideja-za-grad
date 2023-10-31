05d78f53043da4e0fbf2fea1c3b9ced1b859ed4f
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-10-31 23:54:46 +0100

    fix: create marker menu refactored to functional component

--- src/components/CreateMarkerMenu/CreateMarkerMenu.tsx
+++ src/components/CreateMarkerMenu/CreateMarkerMenu.tsx
@@ -4,79 +4,50 @@ import markerType from "../../types/markerTypes";
 import {ImagesUploader} from "../ImagesUploader/ImagesUploader";
 import CloseIcon from '@mui/icons-material/Close';
 
-class CreateMarkerMenu extends React.Component<{
+const CreateMarkerMenu = (props: {
     onClose: Function,
     onSave: Function,
     changeData: Function,
     types: markerType[]
-}, {
-    type: string,
-    description: string,
-    images: Array<File>
-}> {
-    protected wrapperRef = React.createRef<HTMLDivElement>();
-    public width: number = 0;
-    public height: number = 0;
+}) => {
+    const [type, changeType] = React.useState(props.types[0].value)
+    const [images, changeImages] = React.useState<Array<File>>([])
+    const [description, changeDescription] = React.useState('')
 
-    constructor(props: {
-        onClose: Function;
-        onSave: Function;
-        coords: [number, number];
-        changeData: Function;
-        types: markerType[];
-    }) {
-        super(props);
-        this.state = {
-            type: props.types[0].value,
-            description: '',
-            images: []
-        }
-    }
-
-    componentDidMount() {
-        this.width = this.wrapperRef.current?.offsetWidth as number;
-        this.height = this.wrapperRef.current?.offsetHeight as number;
-    }
-
-    render() {
-        return (
-            <div
-                ref={this.wrapperRef}
-                className="create_marker_menu"
+    return (
+        <div className="create_marker_menu">
+            <button className="close_button" onClick={() => props.onClose()}>
+                <CloseIcon/>
+            </button>
+            <select
+                className="menu_element"
+                value={props.types[0] as unknown as string}
+                onChange={e => {
+                    changeType(e.target.value)
+                    props.changeData({type: e.target.value, images, description})
+                }}
             >
-                <button className="close_button" onClick={() => this.props.onClose()}>
-                    <CloseIcon/>
-                </button>
-                <select
-                    className="menu_element"
-                    value={this.state.type}
-                    onChange={e => {
-                        this.setState({type: e.target.value})
-                        this.props.changeData({...this.state, type: e.target.value})
-                    }}
-                >
-                    {this.props.types.map((type, index) => {
-                        return <option key={index} value={type.value}>{type.title}</option>
-                    })}
-                </select>
-                <ImagesUploader
-                    onUpdate={
-                        (images: Array<File>) => {
-                            this.setState({images})
-                            this.props.changeData(
-                                {...this.state, images: images}
-                            )
-                        }
+                {props.types.map((type, index) => {
+                    return <option key={index} value={type.value}>{type.title}</option>
+                })}
+            </select>
+            <ImagesUploader
+                onUpdate={
+                    (images: Array<File>) => {
+                        changeImages(images)
+                        props.changeData(
+                            {type, description, images}
+                        )
                     }
-                />
-                <textarea className="menu_element description_textarea" value={this.state.description} onChange={e => {
-                    this.setState({description: e.target.value})
-                    this.props.changeData({...this.state, description: e.target.value})
-                }}/>
-                <button className="menu_element" onClick={() => this.props.onSave()}>Save</button>
-            </div>
-        )
-    }
+                }
+            />
+            <textarea className="menu_element description_textarea" value={description} onChange={e => {
+                changeDescription(e.target.value)
+                props.changeData({type, description: e.target.value, images})
+            }}/>
+            <button className="menu_element" onClick={() => props.onSave()}>Save</button>
+        </div>
+    )
 }
 
 export default CreateMarkerMenu;
